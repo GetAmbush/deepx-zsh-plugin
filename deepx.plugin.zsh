7e13a64291e20b31b053774ae096f382e98df72d
Daniel Amarante <daniel.amarante2@gmail.com>
2018-07-28 10:24:22 -0300

    Add pair function

--- deepx.plugin.zsh
+++ deepx.plugin.zsh
@@ -6,3 +6,12 @@ function heckshot() {
     adb pull /sdcard/$FILENAME.png $2
     adb shell rm /sdcard/$FILENAME.png
 }
+
+function pair() {
+    if [[ $1 =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+        open vnc://$1
+    else
+        COMPUTER_IP=$(dscacheutil -q host -a name $1.local | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
+        open vnc://$COMPUTER_IP
+    fi
+}
