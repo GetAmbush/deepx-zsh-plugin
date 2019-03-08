fd72a119ad4086a250af54a386a7b73546c6cc43
Fernando Heck <heckfer@gmail.com>
2019-03-08 10:05:54 -0300

    Update pair to accept ip and add heckcast to send broadcast to android apps

--- deepx.plugin.zsh
+++ deepx.plugin.zsh
@@ -7,5 +7,16 @@ function heckshot() {
     adb shell rm /sdcard/$FILENAME.png
 }
 
+# Sends broadcast to app - First param is the broadcast action, second is the app bundle
+function heckcast() {
+    adb shell am broadcast -a $1 -p $2
+}
+
 # Connects to a screen in the local network using Screen Sharing
-function pair () {open vnc://$1}
+function pair() {
+  ip_or_hostname=$1
+  if grep "^[a-zA-Z]" <<(echo $ip_or_hostname); then
+    ip_or_hostname=${ip_or_hostname}.lan
+  fi
+  open vnc://$ip_or_hostname
+}
